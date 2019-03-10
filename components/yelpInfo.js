c1d8e206352e46649782cb5b260a3106c912dddb
Emmy Panken <erpanken@gmail.com>
2019-03-10 18:36:01 -0400

    Feature(React) both the Google Vision API and the Yelp API calls now return valid responses

--- components/yelpInfo.js
+++ components/yelpInfo.js
@@ -1,71 +1,393 @@
-// import React, { Component } from 'react';
-// import { View, Button, StyleSheet, Alert } from 'react-native';
-// import axios from 'axios';
-// import { Constants } from 'expo';
-// import GetLocation from './GetLocation';
+import React from 'react';
+import {
+  ActivityIndicator,
+  Button,
+  Clipboard,
+  FlatList,
+  Image,
+  Share,
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+} from 'react-native';
+import { ImagePicker, Permissions } from 'expo';
+import uuid from 'uuid';
+import Environment from './config/environment';
+import firebase from './config/firebase';
 
-// //need to use the lat/long from the getLocation request as the params in this axios request
+export default class App extends React.Component {
+  state = {
+    image: null,
+    uploading: false,
+    googleResponse: null,
+    yelpResponse: null,
+  };
 
-// const config = {
-//   headers: {
-//     Authorization:
-//       'Bearer NEeaAtk4wztYxtmEmGECM2H8HKNpaj7Fu5kbzWvUkt49n66H2W5NsZI2hfZP5fVp9dEb33lHst7zYpqR48vpG31LbNNQOZmM1BnijmBaawXrqoY3QC-UKOPz24uBXHYx',
-//   },
-//   params: {
-//     longitude: '-122.424051321456', //this should be long from GetLocation
-//     latitude: '37.7614250022004', //this should be lat from GetLocation
-//   },
-// };
+  async componentDidMount() {
+    await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    await Permissions.askAsync(Permissions.CAMERA);
+  }
 
-// export default class YeplInfo extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       yelpInfo: {},
-//       errorMessage: null,
-//     };
+  render() {
+    let { image } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          {/* if image, render image, if not, render Google Cloud Vision */}
+          <View style={styles.getStartedContainer}>
+            {image ? null : (
+              <Text style={styles.getStartedText}>Emmy's App</Text>
+            )}
+          </View>
+
+          <View style={styles.helpContainer}>
+            <Button
+              onPress={this._pickImage}
+              title="Pick an image from camera roll"
+            />
+
+            <Button onPress={this._takePhoto} title="Take a photo" />
+            {this.state.googleResponse && (
+              <FlatList
+                data={this.state.googleResponse.responses[0].labelAnnotations}
+                extraData={this.state}
+                keyExtractor={this._keyExtractor}
+                renderItem={({ item }) => <Text>Item: {item.description}</Text>}
+              />
+            )}
+            {this._maybeRenderImage()}
+            {this._maybeRenderUploadingOverlay()}
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+
+  organize = array => {
+    return array.map(function(item, i) {
+      return (
+        <View key={i}>
+          <Text>
+            {item} {(key = item.id)}
+          </Text>
+        </View>
+      );
+    });
+  };
+
+  _maybeRenderUploadingOverlay = () => {
+    if (this.state.uploading) {
+      return (
+        <View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: 'rgba(0,0,0,0.4)',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+          ]}
+        >
+          <ActivityIndicator color="#fff" animating size="large" />
+        </View>
+      );
+    }
+  };
+
+  _maybeRenderImage = () => {
+    let { image, googleResponse } = this.state;
+    if (!image) {
+      return;
+    }
+
+    let description = null;
+    if (googleResponse)
+      description = googleResponse.responses[0].textAnnotations[0].description.replace(
+        /\s+$/,
+        ''
+      );
+    return (
+      <View
+        style={{
+          marginTop: 20,
+          width: 250,
+          borderRadius: 3,
+          elevation: 2,
+        }}
+      >
+        <Button
+          style={{ marginBottom: 10 }}
+          onPress={() => {
+            this.submitToGoogle();
+            this.submitToYelp;
+          }}
+          title="Analyze!"
+        />
+
+        <View
+          style={{
+            borderTopRightRadius: 3,
+            borderTopLeftRadius: 3,
+            shadowColor: 'rgba(0,0,0,1)',
+            shadowOpacity: 0.2,
+            shadowOffset: { width: 4, height: 4 },
+            shadowRadius: 5,
+            overflow: 'hidden',
+          }}
+        >
+          <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
+        </View>
+        <Text
+          onPress={this._copyToClipboard}
+          onLongPress={this._share}
+          style={{ paddingVertical: 10, paddingHorizontal: 10 }}
+        />
+
+        <Text>Name of restaurant:</Text>
+        {description && (
+          <Text
+            onPress={this._copyToClipboard}
+            onLongPress={this._share}
+            style={{ paddingVertical: 10, paddingHorizontal: 10 }}
+          >
+            {description}
+          </Text>
+        )}
+      </View>
+    );
+  };
+
+  _keyExtractor = (item, index) => item.id;
+
+  _renderItem = item => {
+    <Text>response: {JSON.stringify(item)}</Text>;
+  };
+
+  _share = () => {
+    Share.share({
+      message: JSON.stringify(this.state.googleResponse.responses),
+      title: 'Check it out',
+      url: this.state.image,
+    });
+  };
+
+  _copyToClipboard = () => {
+    Clipboard.setString(this.state.image);
+    alert('Copied to clipboard');
+  };
+
+  _takePhoto = async () => {
+    let pickerResult = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    this._handleImagePicked(pickerResult);
+  };
+
+  _pickImage = async () => {
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    this._handleImagePicked(pickerResult);
+  };
+
+  _handleImagePicked = async pickerResult => {
+    try {
+      this.setState({ uploading: true });
+
+      if (!pickerResult.cancelled) {
+        uploadUrl = await uploadImageAsync(pickerResult.uri);
+        this.setState({ image: uploadUrl });
+      }
+    } catch (e) {
+      console.log(e);
+      alert('Upload failed, sorry :(');
+    } finally {
+      this.setState({ uploading: false });
+    }
+  };
+
+  submitToGoogle = async () => {
+    try {
+      this.setState({ uploading: true });
+      let { image } = this.state;
+      let body = JSON.stringify({
+        requests: [
+          {
+            features: [
+              { type: 'LABEL_DETECTION', maxResults: 10 },
+              { type: 'LANDMARK_DETECTION', maxResults: 5 },
+              { type: 'FACE_DETECTION', maxResults: 5 },
+              { type: 'LOGO_DETECTION', maxResults: 5 },
+              { type: 'TEXT_DETECTION', maxResults: 5 },
+              { type: 'DOCUMENT_TEXT_DETECTION', maxResults: 5 },
+              { type: 'SAFE_SEARCH_DETECTION', maxResults: 5 },
+              { type: 'IMAGE_PROPERTIES', maxResults: 5 },
+              { type: 'CROP_HINTS', maxResults: 5 },
+              { type: 'WEB_DETECTION', maxResults: 5 },
+            ],
+            image: {
+              source: {
+                imageUri:
+                  'https://www.glutenfreefollowme.com/wp-content/uploads/2017/06/Jacks-Wife-Freda-288x219.png',
+              },
+            },
+          },
+        ],
+      });
+      let response = await fetch(
+        'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCLgmQWPMg2Q_PJuzYO2vILWTnmOBLZ_0I',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+
+          body: body,
+        }
+      );
+      let responseJson = await response.json();
+      // console.log(responseJson);
+      this.setState({
+        googleResponse: responseJson,
+        uploading: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
+let yelpResponse;
+const yelpConfig = {
+  headers: {
+    Authorization:
+      'Bearer NEeaAtk4wztYxtmEmGECM2H8HKNpaj7Fu5kbzWvUkt49n66H2W5NsZI2hfZP5fVp9dEb33lHst7zYpqR48vpG31LbNNQOZmM1BnijmBaawXrqoY3QC-UKOPz24uBXHYx',
+  },
+};
+
+submitToYelp = async () => {
+  try {
+    let yelpResponse = await fetch(
+      "https://api.yelp.com/v3/businesses/matches?name=Jack's+Wife+Freda&address1=50+Carmine+St&city=NYC&state=NY&country=US",
+      yelpConfig,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    let yelpResponseJson = await yelpResponse.json();
+    // console.log(responseJson);
+    this.setState({
+      yelpResponse: yelpResponseJson,
+    });
+    console.log(yelpResponseJson);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//upload to firebase~~~~~~~~~
+async function uploadImageAsync(uri) {
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function(e) {
+      console.log(e);
+      reject(new TypeError('Network request failed'));
+    };
+    xhr.responseType = 'blob';
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+  });
+
+  const ref = firebase
+    .storage()
+    .ref()
+    .child(uuid.v4());
+  const snapshot = await ref.put(blob);
+
+  blob.close();
+
+  return await snapshot.ref.getDownloadURL();
+}
+//style ~~~~~~~~~~~~~
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingBottom: 10,
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+});
+
+// const phoneNumber = yelpResponseJson.businesses[0].display_phone
+// const displayAddress = yelpResponseJson.businesses[0].location.display_address
+// yelpResponse Object {
+//     "businesses": Array [
+//       Object {
+//         "alias": "jacks-wife-freda-new-york-2",
+//         "coordinates": Object {
+//           "latitude": 40.7300978230111,
+//           "longitude": -74.0031156668122,
+//         },
+//         "display_phone": "(646) 669-9888",
+//         "id": "HrdIdeDZBTUdyEdSTPG1zg",
+//         "location": Object {
+//           "address1": "50 Carmine St",
+//           "address2": "",
+//           "address3": "",
+//           "city": "New York",
+//           "country": "US",
+//           "display_address": Array [
+//             "50 Carmine St",
+//             "New York, NY 10014",
+//           ],
+//           "state": "NY",
+//           "zip_code": "10014",
+//         },
+//         "name": "Jack's Wife Freda",
+//         "phone": "+16466699888",
+//       },
+//     ],
 //   }
-//   async componentWillMount() {
-//     // const {yelpResponse} = await axios.get('https://api.yelp.com/v3/businesses/search', config)
-//     this.setState({ yelpInfo: yelpResponse });
-//   }
-
-//   _getYelpInfo = async () => {
-//     let { yelpResponse } = await axios.get(
-//       'https://api.yelp.com/v3/businesses/search',
-//       config
-//     );
-//     Alert.alert(yelpResponse);
-
-//     this.setState({
-//       yelpInfo: yelpResponse,
-//     });
-//   };
-
-//   render() {
-//     if (this.state.yelpInfo) {
-//       yelp = JSON.stringify(this.state.yelpInfo);
-//     }
-//     return (
-//       <View style={styles.container}>
-//         {/* <Text style={styles.paragraph}>{text}</Text> */}
-//         <Button onPress={this._getYelpInfo} title="Get my Yelp Info" />
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     paddingTop: Constants.statusBarHeight,
-//     backgroundColor: '#ecf0f1',
-//   },
-//   paragraph: {
-//     margin: 24,
-//     fontSize: 18,
-//     textAlign: 'center',
-//   },
-// });
